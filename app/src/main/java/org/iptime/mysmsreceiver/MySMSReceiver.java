575164b9643432478e879befae0900f1bca5938b
joff <jjhg0406@gmail.com>
2015-06-01 17:26:36 +0900

    https://github.com/jifo/MySMSReceiver.git

---
+++ app/src/main/java/org/iptime/mysmsreceiver/MySMSReceiver.java
@@ -0,0 +1,22 @@
+package org.iptime.mysmsreceiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+public class MySMSReceiver extends BroadcastReceiver {
+    private static final String TAG = "MYSMSReceiver";
+    public MySMSReceiver() {
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        Log.d(TAG, "onReceive() 호출됨");
+
+        Intent myIntent = new Intent(context, MainActivity.class);
+        myIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        context.startActivity(myIntent);
+    }
+}
